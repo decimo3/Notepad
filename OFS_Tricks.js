4e83e2b4a8fc4ac034d9b6955077a3008730c125
Ruan Camello <ruanfaustinocamellodasilva@gmail.com>
2022-08-27 11:15:31 -0300

    a

--- OFS_Tricks.js
+++ OFS_Tricks.js
@@ -1,1 +1,16 @@
-// toa-activities-sortablelist-caption
+// Para rodar essas funções no navegador!
+if (confirm("Deseja acionar qual função?\n[YES] para diminuir cabeçalho\n[NOT] para reduzir a descrição")) {
+  // Função para diminuir o enorme espaço desperdiçado no cabeçalho da página
+  document.getElementsByTagName("controls:page-header")[0].style.padding = "0px";
+} else {
+  // Função para reduzir o texto descritivo das notas no visualizador de mapa
+  var atividades = document.getElementsByClassName("toa-activities-sortablelist-caption");
+  for (let index = 0; index < atividades.length; index++) {
+    var texto = atividades[index].innerText;
+    console.log(texto);
+    var nota = texto.match(/[0-9]{12}/);
+    if(nota){
+      atividades[index].innerText = nota[0];
+    }
+  }
+}
